8b7bf2a9e5a1c2739b6fa2dca5b3843a9ac648f8
Dovydas <dovydas.norkunas@stud.vilniustech.lt>
2025-05-01 21:40:32 +0300

    Added more functions to board

--- checkers-func/board.py
+++ checkers-func/board.py
@@ -1,8 +1,36 @@
+import pygame
 from constants import *
+from piece import PieceFactory
 
 class Board:
     def __init__(self):
         self.board = []
+        self.create_board()
+
+    def draw_squares(self, win):
+        win.fill(WHITE_SQUARES)
+        for row in range(ROWS):
+            for col in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, BLACK_SQUARES, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+    def draw(self, win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(win)
 
     def create_board(self):
-        pass
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if (row + col) % 2 != 0:
+                    if row < 3:
+                        self.board[row].append(PieceFactory.create_piece(row, col, BLACK))
+                    elif row > 4:
+                        self.board[row].append(PieceFactory.create_piece(row, col, WHITE))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
